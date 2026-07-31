5d6eb3be323546ef0aa7b6efd98e2f5f0a696f4a
kunaall09 <itz.kunaall0001@gmail.com>
2026-07-31 22:13:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,13 +70,13 @@ function updateTimer() {
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
 
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+const minutes = Math.floor((diff / (1000 * 60)) % 60);
+const seconds = Math.floor((diff / 1000) % 60);
 
-    const meconds = Math.floor((diff / 1000) % 60);
-
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
+document.getElementById("days").innerText = days;
+document.getElementById("hours").innerText = hours;
+document.getElementById("minutes").innerText = minutes;
+document.getElementById("seconds").innerText = seconds;
 
 }
 
